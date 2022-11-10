248285929ffcf38198ac05cb1b82a0ddc3481268
Arcenio Souza <arcenio_neto@icloud.com>
2022-11-10 09:48:00 -0300

    refactor: :recycle: Altera herança para JpaRepository

--- src/main/java/br/com/lanchebom/models/repository/LancheRepository.java
+++ src/main/java/br/com/lanchebom/models/repository/LancheRepository.java
@@ -1,7 +1,9 @@
 package br.com.lanchebom.models.repository;
 
 import br.com.lanchebom.models.entity.Lanche;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
-public interface LancheRepository extends CrudRepository<Lanche, Integer> {
+public interface LancheRepository extends JpaRepository<Lanche, Integer> {
 }
