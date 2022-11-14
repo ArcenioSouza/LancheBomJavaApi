412edea9b339ff67cd7033c54d993bb4ea15f8e9
Arcenio Souza <arcenio_neto@icloud.com>
2022-11-14 19:03:29 -0300

    docs: :refactor: Altera a visualização de schemas e padroniza responses na documentação

--- src/main/java/br/com/lanchebom/LancheBomJavaApiApplication.java
+++ src/main/java/br/com/lanchebom/LancheBomJavaApiApplication.java
@@ -1,7 +1,29 @@
 package br.com.lanchebom;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "LancheBom_API",
+                description = "Api de gerenciamento de pedidos de uma lanchone",
+                version = "1",
+                contact = @Contact(name = "Arcenio Souza", email = "arcenio_neto@icloud.com")
+        ),
+        servers = @Server(
+                url = "http://localhost:5000",
+                description = "Aprendendo a criar api com Java e Spring Boot"
+        )
+)
 
 @SpringBootApplication
 public class LancheBomJavaApiApplication {
